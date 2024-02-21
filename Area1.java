05164eb037871f3a92360d2f311a71bc991ad5a5
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-21 12:26:10 +0530

    second commit

--- Area1.java
+++ Area1.java
@@ -1,1 +1,13 @@
-#Challeng2
+import java.util.*;
+import java.io;
+
+public class Area1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int height = sc.nextInt();
+    }
+}
